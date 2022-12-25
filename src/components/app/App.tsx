38b28aa423937e74a1967d94cddf48c5255d69e2
patridifusa <p.rufino.a@gmail.com>
2022-12-25 21:44:27 +0000

    Create Menu component and links

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { MenuItems } from "../../types/menu.item";
 import { Footer } from "../footer/footer";
 import { Header } from "../header/header";
 import "./App.css";
 
 function App() {
+    const items: MenuItems = [
+        { path: "/home", label: "Home" },
+        { path: "/robots", label: "Robots" },
+        { path: "/favourites", label: "Favourites" },
+    ];
     return (
         <div className="App">
-            <Header></Header>
+            <Header items={items}></Header>
             <Footer></Footer>
         </div>
     );
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,11 @@
-export function Header() {
+import { MenuItems } from "../../types/menu.item";
+import { Menu } from "../menu/menu";
+
+export function Header({ items }: { items: MenuItems }) {
     return (
         <header>
             <h1>ROBOTS</h1>
+            <Menu items={items}></Menu>
         </header>
     );
 }
