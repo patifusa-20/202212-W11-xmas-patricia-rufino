2c9316730725c7de888f45273d5ff951d983d3af
patridifusa <p.rufino.a@gmail.com>
2022-12-28 16:28:27 +0000

    Create html structure in Home page

--- src/pages/favourites/favourites.page.tsx
+++ src/pages/favourites/favourites.page.tsx
@@ -3,7 +3,7 @@ import { RobotsFav } from "../../components/robots.favourites/favourites";
 export function FavouritesPage() {
     return (
         <>
-            <h2>Robots</h2>
+            <h2>My Favourite Robots</h2>
             <RobotsFav></RobotsFav>
         </>
     );
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,3 +1,10 @@
 export function HomePage() {
-    return <h2>Home</h2>;
+    return (
+        <>
+            <h2>Home</h2>
+            <p>
+                There are <span>25</span> robots available
+            </p>
+        </>
+    );
 }
