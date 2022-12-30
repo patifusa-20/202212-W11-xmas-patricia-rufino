ce11c6d597c1a8fca8e0231acdfb51c10ed430d4
patridifusa <p.rufino.a@gmail.com>
2022-12-30 09:08:58 +0000

    Add prop drilling

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,23 +1,14 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { RobotType } from "../../model/robot.model";
-import { RobotsRepo } from "../../repository/robots.repo";
 import { MenuItems } from "../../types/menu.item";
 
-export function HomePage({ items }: { items: MenuItems }) {
-    const repo = new RobotsRepo();
-    const initialState: Array<RobotType> = [];
-
-    const [robots, setRobots] = useState(initialState);
-
-    const handleLoad = async () => {
-        const data = await repo.load();
-        setRobots(data);
-    };
-
-    useEffect(() => {
-        handleLoad();
-    }, []);
+export function HomePage({
+    items,
+    robots,
+}: {
+    items: MenuItems;
+    robots: Array<RobotType>;
+}) {
     return (
         <>
             <h2>Home</h2>
