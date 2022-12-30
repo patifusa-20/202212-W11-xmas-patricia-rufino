31a8bbc9406394def9a5dff457627ac90e5d24b5
patridifusa <p.rufino.a@gmail.com>
2022-12-30 09:47:32 +0000

    Get and set storage robots

---
+++ src/data/store.robots.ts
@@ -0,0 +1,28 @@
+import { RobotType } from "../model/robot.model";
+import { RobotsRepo } from "../repository/robots.repo";
+
+const repo = new RobotsRepo();
+
+const getStorageList = <T>(storeName: string): Array<T> => {
+    const result = localStorage.getItem(storeName);
+    if (!result) return [];
+    return JSON.parse(result);
+};
+
+const setStorageList = <T>(storeName: string, data: Array<T>): void => {
+    localStorage.setItem(storeName, JSON.stringify(data));
+};
+
+export const getRobots = async (): Promise<Array<RobotType>> => {
+    const data = getStorageList<RobotType>("robots");
+    if (!data.length) {
+        const data = await repo.load();
+        setStorageList("robots", data);
+        return data;
+    }
+    return data;
+};
+
+export const saveRobots = async (robots: Array<RobotType>) => {
+    setStorageList("robots", robots);
+};
